3f15b74fe6095f8a95df2007476cb9b6caecab59
Mads Cordes <Mobilpadde@gmail.com>
2013-10-04 11:58:28 +0200

    Help for deactivated plugins + New plugin
    Ask bot for help for any existing plugin, even though it's not
    activated, and bot shall grant you an explanation why what you're
    trying to do isn't working

--- plugins/help.js
+++ plugins/help.js
@@ -18,14 +18,17 @@ module.exports  = {
       }else{
         plugins.forEach(function(plugin){
           if(plugin.name == message.commands[0]+".js"){
-            //msg = "How to **" + message.commands[0] + "**?  \n" + "**" + plugin.usage + "**: " + plugin.description + ".  \n\n";
-            msg = "How to **" + message.commands[0] + "**, you say?  \n";
-            msg += "Well, it's quite easy to **" + plugin.description.toLowerCase() + "** actually! ";
-            msg += "Simply use this command \"**" + plugin.usage + "**\" and I shall grant you your uttermost desired wish...";
+            if(plugin.activate){
+              msg = "How to **" + message.commands[0] + "**, you say?  \n";
+              msg += "Well, it's quite easy to **" + plugin.description.toLowerCase() + "** actually! ";
+              msg += "Simply use this command \"**" + plugin.usage + "**\" and I shall grant you your uttermost desired wish...";
+            }else{
+              msg = "Well, **" + message.commands[0] + "**.js does exist but haven't been activated by my wonderful master.";
+            }
             return;
           }
         });
-        if(msg){ client.say(msg) }else{ client.say("**404** plugin *"+message.commands[0]+"* could not be found D:") }
+        if(msg){ client.say(msg) }else{ client.say("Plugin **"+message.commands[0]+"**.js could not be found; it might not be activated yet?") }
       }
 
     });
---
+++ plugins/tell.js
@@ -0,0 +1,22 @@
+module.exports  = {
+
+  description : 'Make me say something to a specific user',
+  usage       : 'tell @user %string%',
+  activate    : true,
+
+  register : function(client, plugins){
+  	var that = this;
+
+    client.registerCommand("tell", function(message){
+
+      if(message.commands.length <= 1){
+      	client.say('Usage: '+that.usage);
+        return;
+      }
+
+      client.say(message.commands.join(" "));
+    });
+
+  }
+
+};
